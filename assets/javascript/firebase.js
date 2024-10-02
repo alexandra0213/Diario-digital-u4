30e3692687250eed12f6cc5a1a12209adb6125a4
alexandra0213 <aexl.dx021@gmail.com>
2024-10-01 20:11:35 -0500

    Diario/añadiendo nav

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCd7T4zu8P_WXIkNaruhpvogGYtTN1KWi4",
+  authDomain: "diario-digital-67f4a.firebaseapp.com",
+  projectId: "diario-digital-67f4a",
+  storageBucket: "diario-digital-67f4a.appspot.com",
+  messagingSenderId: "33848349005",
+  appId: "1:33848349005:web:4ea0e619162f72345aa704"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
